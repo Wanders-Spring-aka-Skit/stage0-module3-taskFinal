7dda43faa7db9f02134d2687029cdec264396037
Wanders-Spring-aka-Skit <avasovazik643@gmail.com>
2022-08-27 12:37:05 +0500

    Attempt # 1

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int firstN = number / 1000;
+        int secondN = (number % 1000) / 100;
+        int thirdN = ((number % 1000) % 100) / 10;
+        int fourthN = ((number % 1000) % 100) % 10;
+        System.out.println(firstN + secondN + thirdN + fourthN);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int firstN = number / 100;
+        int secondN = (number % 100) / 10;
+        int third = number % 10;
+        System.out.println(third);
+        System.out.println(secondN);
+        System.out.println(firstN);
     }
 }
